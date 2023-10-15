9627a0876080ec84b5eec440881c81f2cb5a00cf
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-15 21:47:16 +0530

    Inverted number pattern

---
+++ Inverted_Number_Pattern.java
@@ -0,0 +1,16 @@
+public class Inverted_Number_Pattern {
+    public static void main(String[] args){
+        for (int i=5;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+
+//        for (int a=1;a<=5;a++){
+//            for (int b=1;b<=10;b++){
+//                System.out.println(b);
+//            }
+//        }
+    }
+}
