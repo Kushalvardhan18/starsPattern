9a804ac9367354efc91e14cb4056936cf0a374b2
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-30 22:09:31 +0530

    binary 0,1 triangle pattern

--- BinaryTrianglePattern.java
+++ BinaryTrianglePattern.java
@@ -1,2 +1,18 @@
 public class BinaryTrianglePattern {
+    public static void trianglePattern(int n){
+        for (int i =1;i<=n;i++){
+            for (int j = 1;j<=i;j++){
+                if ((i+j) %2 ==0){
+                    System.out.print(1);
+                }
+                else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args){
+        trianglePattern(5);
+    }
 }
