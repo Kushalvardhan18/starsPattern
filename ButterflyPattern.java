10499095030e2835bc93dcfaa16208fbad71f4d0
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-31 16:21:59 +0530

    butterfly pattern

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -1,2 +1,33 @@
 public class ButterflyPattern {
+    public static void pattern(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= (n * 2) - (i * 2); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i=n;i>=1;i--){
+            for (int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            for (int k =(n * 2) - (i * 2);k>=1;k--){
+                System.out.print(" ");
+            }
+            for (int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern(4);
+    }
 }
