471fc8b1c8836cb3e1b436b82a0de7b8888c8595
Kushal Vardhan <vardhank140102@gmail.com>
2023-11-01 14:53:39 +0530

    Create Solid_Rhombus.java

---
+++ Solid_Rhombus.java
@@ -0,0 +1,10 @@
+public class Solid_Rhombus {
+    public static void pattern(int n){
+        for (int i=1;i<=n;i++){
+            for (int )
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
