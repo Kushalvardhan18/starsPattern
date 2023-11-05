13f36143a5fd12b3dacef7e0ea18036642676abb
Kushal Vardhan <vardhank140102@gmail.com>
2023-11-05 20:42:51 +0530

    hollow rhombus

--- Solid_Rhombus.java
+++ Solid_Rhombus.java
@@ -1,10 +1,3 @@
 public class Solid_Rhombus {
-    public static void pattern(int n){
-        for (int i=1;i<=n;i++){
-            for (int )
-        }
-    }
-    public static void main(String[] args){
 
-    }
 }
